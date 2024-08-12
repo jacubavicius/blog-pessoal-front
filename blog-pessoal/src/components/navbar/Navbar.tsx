0a7f69834707107a30760ef8629301555fac8fa3
gabriella_jacubavicius@hotmail.com <gabriella_jacubavicius@hotmail.com>
2024-08-12 10:46:30 -0300

    style: estilização background, text

--- blog-pessoal/src/components/navbar/Navbar.tsx
+++ blog-pessoal/src/components/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
 
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+     <div className='w-full bg-azul-lago text-mica-negra flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
             <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
 
--- blog-pessoal/tailwind.config.js
+++ blog-pessoal/tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-lago': '#add4d3',
+        'mica-negra': '#333237',
+        'soft': '#e9e2da',
+        'citrus': '#ffad64',
+        'capadocia': '#fb8351'
+      },
+    },
   },
   plugins: [],
 }
