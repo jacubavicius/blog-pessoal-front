36978f9ccf21e2819dd7ae5be48e0e43534c75d5
gabriella_jacubavicius@hotmail.com <gabriella_jacubavicius@hotmail.com>
2024-08-12 10:11:14 -0300

    feat: criando rotas com o React Router Dom

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
+}
+
+export default App;
