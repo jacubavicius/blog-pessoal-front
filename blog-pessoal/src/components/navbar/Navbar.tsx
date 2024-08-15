43ab5b558c14646cbbbe28a9b33e23f81630e145
gabriella_jacubavicius@hotmail.com <gabriella_jacubavicius@hotmail.com>
2024-08-15 18:30:27 -0300

    feat: crud postagem

--- blog-pessoal/src/components/navbar/Navbar.tsx
+++ blog-pessoal/src/components/navbar/Navbar.tsx
@@ -1,40 +1,37 @@
-import React, { useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../contexts/AuthContext'
-
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
-  const { usuario, handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
-      handleLogout()
-      alert('Usuário deslogado com sucesso')
-      navigate('/login')
+    handleLogout();
+    alert("Usuário deslogado com sucesso");
+    navigate("/login");
   }
 
-  let navbarComponent
-  
+  let navbarComponent;
 
   return (
     <>
       <div className="w-full bg-sky-200 text-slat-950 flex justify-center py-4">
         <div className="container flex justify-between text-xl">
-          <div className="text-2xl font-bold uppercase">Destinos Incríveis</div>
-          
-          <div className='flex gap-4'>
-            <div className='hover:underline'>Postagens</div>
-            <Link to='/temas' className='hover:underline'>Temas</Link>
-            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-              <div className='hover:underline'>Perfil</div>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-             
-            </div>
+          <Link to='/home' className='text-2xl font-bold uppercase'>Destinos Incríveis</Link>
+
+          <div className="flex gap-4">
+            <Link to="/postagens" className="hover:underline">Postagens</Link>
+            <Link to="/temas" className="hover:underline">Temas</Link>
+            <Link to="/cadastroTema" className="hover:underline">Cadastrar tema</Link>
+            <div className="hover:underline">Perfil</div>
+            <Link to="" onClick={logout} className="hover:underline">Sair</Link>
           </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- blog-pessoal/src/pages/home/Home.tsx
+++ blog-pessoal/src/pages/home/Home.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import home from '../../assets/home.png'
-import './Home.css';
-
+import React from "react";
+import home from "../../assets/home.png";
+import "./Home.css";
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
 
 function Home() {
-    return (
-        <>
-       <div className="bg-sky-200 flex justify-center">
-          <div className='container grid grid-cols-2 text-slat-950'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Bem-vindo a Bordo!</h2>
-              <p className='text-xl'>Descubra o mundo, um destino de cada vez.</p>
-  
-              <div className="flex justify-around gap-4">
-              
-                <button className='text-white bg-sky-400 hover:bg-sky-800 py-2 px-4'>Ver postagens</button>
-              </div>
-            </div>
-  
-            <div className="flex justify-center ">
-              <img src={home} alt="" className='w-2/3' />
-      
+  return (
+    <>
+      <div className="bg-sky-200 flex justify-center">
+        <div className="container grid grid-cols-2 text-slat-950">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className="text-5xl font-bold">Bem-vindo a Bordo!</h2>
+            <p className="text-xl">Descubra o mundo, um destino de cada vez.</p>
+
+            <div className="flex justify-around gap-4">
+              <ModalPostagem />
+              <button className="text-white bg-sky-400 hover:bg-sky-800 py-2 px-4">Ver postagens</button>
             </div>
           </div>
+
+          <div className="flex justify-center ">
+            <img src={home} alt="" className="w-2/3" />
+          </div>
         </div>
-      </>
-    );
+      </div>
+      <ListaPostagens />
+    </>
+  );
 }
 
 export default Home;
